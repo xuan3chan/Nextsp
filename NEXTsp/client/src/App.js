a10b2033cde2d59a88f1ed8f54b35d5650c0f630
Thaitam <letranthaitam3000@gmail.com>
2023-10-08 19:57:27 +0700

    push again

--- NEXTsp/client/src/App.js
+++ NEXTsp/client/src/App.js
@@ -3,13 +3,15 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Landing } from './components'
 import Auth from './views/Auth'
 import { LoginForm, RegisterFrom } from './components';
+import Homepage from './container/Homepage';
 
 const App = () => {
   return (
     <>   
       <Router>
         <Routes>
-          <Route path="/Homepage" element={<Landing />} />
+          <Route path="/" element={<Landing />} />
+          <Route path="/Homepage" element={<Homepage />} />
           <Route path='/login' element={<LoginForm/>}/>
           <Route path="/register" element={<RegisterFrom/>} />
         </Routes>
