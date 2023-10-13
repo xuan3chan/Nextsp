472f218324aeac9806882ccc155f5f0013eccdd9
Thaitam <letranthaitam3000@gmail.com>
2023-10-13 17:35:03 +0700

    add banner sales
    Thêm font lato hoàn thành banner sales

---
+++ NEXTsp/client-customer/src/components/homepage/BannerSales.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import bgBannerSales from '../../assets/img/Apple-Iphone-15-Pro-PNG.png'
+const BannerSales = () => {
+
+  return (
+    <div className='w-full h-[490px] bg-[#EDF1F3] relative flex justify-center items-center'>
+      <img src={bgBannerSales} alt="SALES"
+        className='absolute w-[600px] right-0 bottom-0 px-5 ' />
+      <div className='w-[900px]'>
+        <p className='text-2xl'>Giảm giá 10%</p>
+        <h1 className='text-6xl font-lato-light font-light mt-2'>Các mặt hàng iphone</h1>
+        <button className='px-8 py-2 mt-10 hover:bg-gray-800 bg-black text-white'>Mua ngay</button>
+      </div>
+    </div>
+  )
+}
+
+export default BannerSales
--- NEXTsp/client-customer/src/components/index.js
+++ NEXTsp/client-customer/src/components/index.js
@@ -3,6 +3,7 @@ export { default as RegisterFrom } from './auth/RegisterFrom';
 export { default as LoginForm } from './auth/LoginForm';
 export { default as BannerTop } from './homepage/BannerTop';
 export { default as Header } from './homepage/Header';
+export { default as BannerSales } from './homepage/BannerSales';
 
 
 
