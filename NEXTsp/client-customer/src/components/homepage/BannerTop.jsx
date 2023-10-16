a64d0f98481e79514fbcb92d4a84c1cba5701cd0
Thaitam <letranthaitam3000@gmail.com>
2023-10-16 11:42:55 +0700

    Sửa header homepage

--- NEXTsp/client-customer/src/components/homepage/BannerTop.jsx
+++ NEXTsp/client-customer/src/components/homepage/BannerTop.jsx
@@ -5,7 +5,7 @@ import bannerTop from '../../assets/img/banner_top.png';
 function BannerTop(props) {
     return (
         <div>
-        <img src={bannerTop} alt="" className="banner_top" />
+        <img src={bannerTop} alt="" className="banner_top z-30 fixed top-0" />
         </div>
     );
 }
