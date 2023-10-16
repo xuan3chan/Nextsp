fed5ac0b63782c3eb01248bbb471a1b540e93a47
Thaitam <letranthaitam3000@gmail.com>
2023-10-16 11:42:55 +0700

    Sửa header homepage

--- NEXTsp/client-customer/src/container/Homepage.jsx
+++ NEXTsp/client-customer/src/container/Homepage.jsx
@@ -1,20 +1,20 @@
 import React from 'react';
-import { BannerTop, Header, BannerSales, BlogSection,BannerScroll,SloganListSection,ProductList } from '../components'; 
+import { Header, BannerSales, BlogSection, BannerScroll,SloganListSection,ProductList, Footer} from '../components'; 
 import '../assets/css/homepage.css';
 function Homepage(props) {
     return (
         <div>
-          <BannerTop></BannerTop>
           <Header></Header>
-          <div className="container_content">
-          <BannerScroll/>
-          <SloganListSection/>
-          <ProductList></ProductList>
-          <ProductList></ProductList>
-          
-          <BannerSales/>
-          <BlogSection/>
+          <div className="container_content mt-[56px]">
+            <BannerScroll/>
+            <SloganListSection/>
+            <ProductList></ProductList>
+            <ProductList></ProductList>
+            
+            <BannerSales/>
+            <BlogSection/>
           </div>
+          <Footer/>
         </div>
     );
 }
