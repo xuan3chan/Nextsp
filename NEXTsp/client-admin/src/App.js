fc7983e3f022b2d605986f627f8939241b8aaa8e
Thaitam <letranthaitam3000@gmail.com>
2023-10-16 16:29:34 +0700

    push cái gì hết nhớ òi

--- NEXTsp/client-admin/src/App.js
+++ NEXTsp/client-admin/src/App.js
@@ -1,8 +1,13 @@
 import React from 'react'
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
 
 const App = () => {
   return (
-    <div>App</div>
+    <Router>
+      <Routes>
+        
+      </Routes>
+    </Router>
   )
 }
 
