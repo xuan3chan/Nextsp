649b1dd310521a3fca3747e2b37472b5471fb314
Thaitam <letranthaitam3000@gmail.com>
2023-10-17 18:27:20 +0700

    add login admin

--- NEXTsp/client-admin/src/App.js
+++ NEXTsp/client-admin/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
+import { Login } from './components'
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        
+        <Route path="/" element={<Login/>} />
+        {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
       </Routes>
     </Router>
   )
