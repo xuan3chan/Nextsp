789c177f2126390678a515fd9cab0f241b311009
Thaitam <letranthaitam3000@gmail.com>
2023-10-18 16:33:30 +0700

    add login and some dashboard

--- NEXTsp/client-admin/src/App.js
+++ NEXTsp/client-admin/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom'
-import { Login } from './components'
-
+import { Login, AdminLayout } from './components'
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Login/>} />
-        {/* <Route path="/dashboard" element={<Dashboard/>} /> */}
+        <Route path="/" element={<AdminLayout/>} />
+        <Route path='/Login' element={<Login/>} />
+        {/* <Route path="/Dashboard" element={<Dashboard/>} /> */}
       </Routes>
     </Router>
   )
--- NEXTsp/client-admin/src/container/Dashboard/Home.jsx
+++ NEXTsp/client-admin/src/container/Dashboard/Home.jsx
@@ -1,11 +1,11 @@
-import React from 'react';
+import React from 'react'
 
-function Home(props) {
-    return (
-        <div>
-            
-        </div>
-    );
+const Home = () => {
+  return (
+    <div>
+      home
+    </div>
+  )
 }
 
-export default Home;
+export default Home
