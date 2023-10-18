67a29ec5d443ca1e75b3b38211dacbb1eda45e88
Thaitam <letranthaitam3000@gmail.com>
2023-10-18 17:20:58 +0700

    push admin layout

--- NEXTsp/client-admin/tailwind.config.js
+++ NEXTsp/client-admin/tailwind.config.js
@@ -1,32 +1,32 @@
-// /** @type {import('tailwindcss').Config} */
-// module.exports = {
-//   content: [
-//     "src/**/*.{js,jsx,html}",
-//     "src/components/.{js,jsx,html}",
-//     "./src/components/.{html,js,jsx}",
-//     "./App.js/.{js,jsx}",
-//     ".public/index.html",
-//     "src/components/*/.{js,jsx,html}",
-//     './pages/**/*.{html,js}',
-//     './components/**/*.{html,js,jsx}'
-//   ],
-//   theme: {
-//     extend: {
-//       colors: {
-//         'login-pink': '#FFF3FF',
-//         'login-blue': '#CBE1FD',
-//         'login-green': '#DAFFE6',
-//         'login-white': '#ffffff'
-//       },
-//       height: {
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "src/**/*.{js,jsx,html}",
+    "src/components/.{js,jsx,html}",
+    "./src/components/.{html,js,jsx}",
+    "./App.js/.{js,jsx}",
+    ".public/index.html",
+    "src/components/*/.{js,jsx,html}",
+    './pages/**/*.{html,js}',
+    './components/**/*.{html,js,jsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'login-pink': '#FFF3FF',
+        'login-blue': '#CBE1FD',
+        'login-green': '#DAFFE6',
+        'login-white': '#ffffff'
+      },
+      height: {
         
-//       }
-//     },
-//     '@media (min-width: 1024px)': {
-//       '.lg\\:block': {
-//         display: 'block',
-//       },
-//     },
-//   },
-//   plugins: [],
-// };
+      }
+    },
+    '@media (min-width: 1024px)': {
+      '.lg\\:block': {
+        display: 'block',
+      },
+    },
+  },
+  plugins: [],
+};
