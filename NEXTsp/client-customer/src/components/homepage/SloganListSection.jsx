4af3d71ccd4d8908cca2d8ea0f001170d5ac309e
Thieen <thienctv03@gmail.com>
2023-10-18 18:04:45 +0700

    Update collection page

--- NEXTsp/client-customer/src/components/homepage/SloganListSection.jsx
+++ NEXTsp/client-customer/src/components/homepage/SloganListSection.jsx
@@ -1,17 +1,26 @@
-import React from 'react';
-import SloganItem from './SloganItem';
+import React from "react";
+import SloganItem from "./SloganItem";
 import "../../assets/css/homepage.css";
 function SloganListSection(props) {
-    return (
-        <div className='flex items-center bg-w'>
-            <div className="SloganListSection w-3/4 flex flex-wrap justify-center items-center ml-auto mr-auto">
-            <SloganItem title="Giao hàng miễn phí" desc="Cho đơn hàng từ 500.000đ trở lên"/>
-            <SloganItem title="Hỗ trợ 24/7" desc="Hotline: 1900 1000"/>
-            <SloganItem title="Giảm giá 20%" desc="Cho đơn hàng từ 500.000đ trở lên"/>
-            <SloganItem title="Bảo Mật Thông Tin" desc="Đảm bảo thông tin khách hàng được bảo mật"/>
-            </div>
-        </div>
-    );
+  return (
+    <div className="flex items-center bg-w">
+      <div className="SloganListSection flex flex-wrap ">
+        <SloganItem
+          title="Giao hàng miễn phí"
+          desc="Cho đơn hàng từ 500.000đ trở lên"
+        />
+        <SloganItem title="Hỗ trợ 24/7" desc="Hotline: 1900 1000" />
+        <SloganItem
+          title="Giảm giá 20%"
+          desc="Cho đơn hàng từ 500.000đ trở lên"
+        />
+        <SloganItem
+          title="Bảo Mật Thông Tin"
+          desc="Đảm bảo thông tin khách hàng được bảo mật"
+        />
+      </div>
+    </div>
+  );
 }
 
 export default SloganListSection;
