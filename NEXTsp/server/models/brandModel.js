5d487ca769bff939becb59de152099d48418a55b
xuandd <103272537+xuan3chan@users.noreply.github.com>
2023-10-18 14:54:03 +0700

    xuanpush api loginadmin

--- NEXTsp/server/models/brandModel.js
+++ NEXTsp/server/models/brandModel.js
@@ -1,1 +1,20 @@
 // model brand 
+const mongoose = require('mongoose');
+const brandSchema = new mongoose.Schema(
+    {
+        nameBrand: {
+            type: String,
+            required: true,
+            trim: true,
+            unique: true,
+        },
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+    },
+    {
+        timestamps: true, //important
+    }
+);
