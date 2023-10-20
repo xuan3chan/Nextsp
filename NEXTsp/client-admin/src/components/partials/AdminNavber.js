d19f7293bfee65b2a3f8c459eec27d057086a6ce
Thaitam <letranthaitam3000@gmail.com>
2023-10-20 15:02:53 +0700

    complete login logout admin

--- NEXTsp/client-admin/src/components/partials/AdminNavber.js
+++ NEXTsp/client-admin/src/components/partials/AdminNavber.js
@@ -2,15 +2,12 @@ import React, { Fragment } from "react";
 import { useNavigate  } from "react-router-dom";
 
 const AdminNavber = (props) => {
-  const history = useNavigate ();
+  const navigate = useNavigate ();
 
   const logout = () => {
-    localStorage.removeItem("jwt");
-    localStorage.removeItem("cart");
-    localStorage.removeItem("wishList");
-    window.location.href = "/Login";
+    localStorage.removeItem("accessToken");
+    navigate("/login");
   };
-
   return (
     <Fragment>
       <nav className="sticky z-10 flex items-center shadow-md justify-between px-4 py-4 md:px-8 top-0 w-full bg-white">
@@ -36,7 +33,7 @@ const AdminNavber = (props) => {
         {/*  Large Screen Show  */}
         <div className="hidden lg:block">
           <span
-            onClick={(e) => history.push("/admin/dashboard")}
+            onClick={(e) => navigate("/admin/dashboard")}
             style={{ letterSpacing: "0.70rem" }}
             className="flex items-left text-center font-bold uppercase text-gray-800 text-2xl cursor-pointer px-2 text-center"
           >
@@ -61,7 +58,7 @@ const AdminNavber = (props) => {
             />
           </svg>
           <span
-            onClick={(e) => history.push("/admin/dashboard")}
+            onClick={(e) => navigate("/admin/dashboard")}
             style={{ letterSpacing: "0.10rem" }}
             className="flex items-left text-center font-bold uppercase text-gray-800 text-2xl cursor-pointer px-2 text-center"
           >
@@ -104,8 +101,8 @@ const AdminNavber = (props) => {
           </div>
           {/* Logout Button Dropdown */}
           <div
-            className="userDropdownBtn hover:bg-gray-200 px-2 py-2 rounded-lg relative"
-            title="Logout"
+            className="userDropdownBtn hover:bg-gray-200 px-2 py-2 rounded-lg relative group"
+            title="user"
           >
             <svg
               className="cursor-pointer w-8 h-8 text-gray-600 hover:text-gray-800"
@@ -121,30 +118,8 @@ const AdminNavber = (props) => {
                 d="M5.121 17.804A13.937 13.937 0 0112 16c2.5 0 4.847.655 6.879 1.804M15 10a3 3 0 11-6 0 3 3 0 016 0zm6 2a9 9 0 11-18 0 9 9 0 0118 0z"
               />
             </svg>
-            <div className="userDropdown absolute right-0 mt-1 bg-gray-200 rounded">
+            <div className="hidden userDropdown absolute right-0 mt-1 bg-gray-200 rounded group-hover:block">
               <li className="flex flex-col text-gray-700">
-                <span
-                  // onClick={(e) => history.push("/")}
-                  className="flex space-x-1 py-2 px-8 hover:bg-gray-400 cursor-pointer"
-                >
-                  <span>
-                    <svg
-                      className="w-6 h-6"
-                      fill="none"
-                      stroke="currentColor"
-                      viewBox="0 0 24 24"
-                      xmlns="http://www.w3.org/2000/svg"
-                    >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth={2}
-                        d="M16 11V7a4 4 0 00-8 0v4M5 9h14l1 12H4L5 9z"
-                      />
-                    </svg>
-                  </span>
-                  <span>Shop</span>
-                </span>
                 <span className="flex space-x-1 py-2 px-8 hover:bg-gray-400 cursor-pointer">
                   <span>
                     <svg
@@ -173,6 +148,7 @@ const AdminNavber = (props) => {
                 <span
                   onClick={(e) => logout()}
                   className="flex space-x-1 py-2 px-8 hover:bg-gray-400 cursor-pointer"
+                  title="Logout"
                 >
                   <span>
                     <svg
