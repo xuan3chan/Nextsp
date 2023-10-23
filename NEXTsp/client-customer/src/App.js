170343abd8010fd73c1b00fbcf7c181e6203df2b
Thieen <thienctv03@gmail.com>
2023-10-23 17:03:12 +0700

    Fixed

--- NEXTsp/client-customer/src/App.js
+++ NEXTsp/client-customer/src/App.js
@@ -1,82 +1,33 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-import React from 'react';
-=======
-import React, { useEffect } from 'react';
->>>>>>> 4d72f579d0da82d2ea58f16fcf48cf514b99e118
-=======
-import React, { useEffect } from 'react';
-=======
-import React from 'react';
->>>>>>> d41284d94d63dd98856c2c8e2e05f4c90e7e900c
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { Landing } from './components'
-import { LoginForm, RegisterFrom} from './components';
-import Homepage from './container/Homepage';
-import Collection from '../src/container/Collections/Collection'
-<<<<<<< HEAD
-<<<<<<< HEAD
-import laptop  from '../src/container/Collections/CollectionLaptop'
+import React, { useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Landing } from "./components";
+import { LoginForm, RegisterFrom } from "./components";
+import Homepage from "./container/Homepage";
+import Collection from "../src/container/Collections/Collection";
+import CollectionLaptop from "../src/container/Collections/CollectionLaptop";
+import CollectionPc from "../src/container/Collections/CollectionPC";
+import CollectionGear from "../src/container/Collections/CollectionGear";
+import CollectionAccessory from "../src/container/Collections/CollectionAccessory";
+import { LoadingProvider } from "./components/LoadingContext";
 const App = () => {
-  return (   
+  return (
     <Router>
-      <Routes>
-        <Route path="/Homepage" element={<Homepage />} />
-        {/* <Route path="/Homepage/laptop" element={<SloganListSection/>} /> */}
-        <Route path="/" element={<Landing />} />
-        <Route path='/login' element={<LoginForm/>}/>
-        <Route path="/register" element={<RegisterFrom/>} />
-        <Route path='/collection' element={<Collection/>}/>
-        <Route path='/collection/laptop' element={<Collection/>}/>
-      </Routes>
-=======
-=======
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
-import CollectionLaptop  from '../src/container/Collections/CollectionLaptop'
-import CollectionPc  from '../src/container/Collections/CollectionPC'
-import CollectionGear from '../src/container/Collections/CollectionGear'
-import CollectionAccessory from '../src/container/Collections/CollectionAccessory'
-import { LoadingProvider } from './components/LoadingContext';
-const App = () => {
-
-  return (   
-    <Router>
-    <LoadingProvider>
-    <Routes>
-      <Route path="/Homepage" element={<Homepage />} />
-      {/* <Route path="/Homepage/laptop" element={<SloganListSection/>} /> */}
-      <Route path="/" element={<Landing />} />
-      <Route path='/login' element={<LoginForm/>}/>
-      <Route path="/register" element={<RegisterFrom/>} />
-      <Route path='/collection' element={<Collection/>}/>
-      <Route path='/collection/laptop' element={<CollectionLaptop/>}/>
-      <Route path='/collection/PC' element={<CollectionPc/>}/>
-      <Route path='/collection/LinhKien' element={<CollectionGear/>}/>
-      <Route path='/collection/PhuKien' element={<CollectionAccessory/>}/>
-    </Routes>
-    </LoadingProvider>
-<<<<<<< HEAD
->>>>>>> 4d72f579d0da82d2ea58f16fcf48cf514b99e118
-=======
-=======
-import laptop  from '../src/container/Collections/CollectionLaptop'
-const App = () => {
-  return (   
-    <Router>
-      <Routes>
-        <Route path="/Homepage" element={<Homepage />} />
-        {/* <Route path="/Homepage/laptop" element={<SloganListSection/>} /> */}
-        <Route path="/" element={<Landing />} />
-        <Route path='/login' element={<LoginForm/>}/>
-        <Route path="/register" element={<RegisterFrom/>} />
-        <Route path='/collection' element={<Collection/>}/>
-        <Route path='/collection/laptop' element={<Collection/>}/>
-      </Routes>
->>>>>>> d41284d94d63dd98856c2c8e2e05f4c90e7e900c
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
+      <LoadingProvider>
+        <Routes>
+          <Route path="/Homepage" element={<Homepage />} />
+          {/* <Route path="/Homepage/laptop" element={<SloganListSection/>} /> */}
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<LoginForm />} />
+          <Route path="/register" element={<RegisterFrom />} />
+          <Route path="/collection" element={<Collection />} />
+          <Route path="/collection/laptop" element={<CollectionLaptop />} />
+          <Route path="/collection/PC" element={<CollectionPc />} />
+          <Route path="/collection/LinhKien" element={<CollectionGear />} />
+          <Route path="/collection/PhuKien" element={<CollectionAccessory />} />
+        </Routes>
+      </LoadingProvider>
     </Router>
-  )
-}
+  );
+};
 
 export default App;
--- NEXTsp/client-customer/src/components/homepage/ProductList.jsx
+++ NEXTsp/client-customer/src/components/homepage/ProductList.jsx
@@ -1,19 +1,6 @@
 import React from "react";
 import axios from "axios";
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-
-
-function ProductList(props) {
-  const [products, setProducts] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get("http://localhost:3003/Products");
-=======
-=======
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
 import {FaTruckMoving} from "react-icons/fa";
 import {BiSolidRightArrow}  from "react-icons/bi";
 function ProductList(props) {
@@ -23,21 +10,6 @@ function ProductList(props) {
   React.useEffect(() => {
     const fetchData = async () => {
       const result = await axios.get(ApiProducts);
-<<<<<<< HEAD
->>>>>>> 4d72f579d0da82d2ea58f16fcf48cf514b99e118
-=======
-=======
-
-
-
-function ProductList(props) {
-  const [products, setProducts] = React.useState([]);
-
-  React.useEffect(() => {
-    const fetchData = async () => {
-      const result = await axios.get("http://localhost:3003/Products");
->>>>>>> d41284d94d63dd98856c2c8e2e05f4c90e7e900c
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
 
       setProducts(result.data);
     };
@@ -48,28 +20,11 @@ function ProductList(props) {
     return `${price.toLocaleString()}đ`;
   }
   return (
-<<<<<<< HEAD
-<<<<<<< HEAD
-    <div className="productList p-12 mr-auto ml-auto bg-white rounded-md">
-      <h2 className="titleList mb-4 text-center">{props.title}</h2>
-=======
-=======
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
-    <div className="productList p-4 mr-auto ml-auto bg-white rounded-md">
-      <div className="TitleSection ml-6 h-16 flex items-center relative">
-        <h2 className="titleList text-left">{props.title}</h2>
-        <h3 className="subTitleList flex justify-center items-center ml-6 gap-2" ><FaTruckMoving></FaTruckMoving>  Miễn Phí Giao Hàng</h3>
-        <a href = {`/Collection/${props.CollectionName}`} className="MoreInfro  absolute right-3 flex items-center gap-2"><p>Xem Thêm</p><BiSolidRightArrow></BiSolidRightArrow></a>
-        </div>
 
-<<<<<<< HEAD
->>>>>>> 4d72f579d0da82d2ea58f16fcf48cf514b99e118
-=======
-=======
+
+
     <div className="productList p-12 mr-auto ml-auto bg-white rounded-md">
       <h2 className="titleList mb-4 text-center">{props.title}</h2>
->>>>>>> d41284d94d63dd98856c2c8e2e05f4c90e7e900c
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
       <div className=" flex w-max flex-wrap gap-4 content-center justify-center">
         {products.map(
           (product, index) =>
@@ -78,9 +33,6 @@ function ProductList(props) {
                 key={product.id}
                 className="productItem flex flex-col  border-black-500/100 p-4 gap-1 "
               >
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
                 <div className="product_image w-72 h-52 object-contain">
                   <img
                     src={product.imageUrl}
@@ -100,56 +52,6 @@ function ProductList(props) {
                   </p>
                 </div>
                 <div className="over-button flex gap-4 items-center justify-center mt-3">
-=======
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
-              <div className="product_image w-72 h-52 object-contain">
-              <img
-                src={product.imageUrl}
-                alt=""
-                className="w-full h-44 object-contain "
-              />
-              </div>
-              <div className ="product_title">
-              <h1 className=" ">{product.title} </h1>
-              </div>
-                <div>
-                  <p className="product_oldPrice font-bold RobotoViet">{formatPrice(product.oldPrice)}</p>
-                  <p className="product_price font-normal RobotoViet">{formatPrice(product.price)}</p>
-                </div>
-                <div className="product_rating flex gap-1 items-center">
-                  <p>⭐</p>
-                  <p>⭐</p>
-                  <p>⭐</p>
-                  <p>⭐</p>
-                  <p>⭐</p>
-                  <p className="text-xs	">(5 đánh giá)</p>
-                </div>
-                <div className="over-button flex gap-4 items-center justify-center">
-<<<<<<< HEAD
-=======
-                <div className="product_image w-72 h-52 object-contain">
-                  <img
-                    src={product.imageUrl}
-                    alt=""
-                    className="w-full h-44 object-contain "
-                  />
-                </div>
-                <div className="product_title">
-                  <h1 className=" ">{product.title} </h1>
-                </div>
-                <div>
-                  <p className="product_oldPrice font-bold RobotoViet">
-                    {formatPrice(product.oldPrice)}
-                  </p>
-                  <p className="product_price font-normal RobotoViet">
-                    {formatPrice(product.price)}
-                  </p>
-                </div>
-                <div className="over-button flex gap-4 items-center justify-center mt-3">
->>>>>>> 4d72f579d0da82d2ea58f16fcf48cf514b99e118
-=======
->>>>>>> d41284d94d63dd98856c2c8e2e05f4c90e7e900c
->>>>>>> cf830381dfc55af02a0186b8d0852ca2e5b7a295
                   <div className="btn p-1  flex justify-center btn-sell ">
                     Mua Ngay
                   </div>
