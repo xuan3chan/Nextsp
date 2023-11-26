39d9ff3045058e29c85ce584788ccc05af4c1e24
ThieenJ <94862916+thieenjdev03@users.noreply.github.com>
2023-11-27 00:18:37 +0700

    gg

--- NEXTsp/client-customer/src/container/Products.jsx
+++ NEXTsp/client-customer/src/container/Products.jsx
@@ -5,29 +5,20 @@ import { Breadcrumb } from "../components";
 import ImageSection from "../components/Products/ImageSection";
 import MainContentSection from "../components/Products/MainContentSection";
 import DescriptionSection from "../components/Products/DescriptionSection";
-import FlaskSale from "../components/Products/FlaskSale";
 import ReviewForm from "../components/Products/ReviewForm";
 import GetReview from "../components/Products/GetReview";
-import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import axios from "axios";
 
 function Products(props) {
-  const handleReviewSubmit = (reviewData) => {
-    // Xử lý đánh giá, ví dụ: gửi đến máy chủ hoặc lưu vào trạng thái ứng dụng
-    console.log("Đánh giá đã được gửi:", reviewData);
-  };
-
   return (
     <div className="bg-product w-full ">
       <Header></Header>
       <div className="">
-        <div className="container flex flex-col w-3/4 mx-auto h-full  ">
+        <div className="wrapper relative flex flex-col w-3/4 mx-auto h-full  ">
           <Breadcrumb titleCollection="Sản Phẩm"></Breadcrumb>
           <div className="flex bg-white w-full rounded-md h-full mb-8 pl-28 pb-10">
             <div>
               <ImageSection></ImageSection>
-              <ReviewForm onSubmit={handleReviewSubmit} />
+              <ReviewForm />
             </div>
             <div className="flex flex-col gap-8">
               <MainContentSection></MainContentSection>
