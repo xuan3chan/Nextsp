931e875863b4b04f2e7dc438ee4f29a699f6389e
ThieenJ <94862916+thieenjdev03@users.noreply.github.com>
2023-11-28 14:15:09 +0700

    hi

--- NEXTsp/client-customer/src/components/auth/RegisterFrom.jsx
+++ NEXTsp/client-customer/src/components/auth/RegisterFrom.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from "react";
-import { Link, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from "react-router-dom";
 import "../../assets/css/registerFrom.css";
 import axios from "axios";
-
+import Logo from "../../assets/img/Logo.png";
+import Footer from "../../components/Footer";
 const RegisterForm = () => {
   const apiUrl = "https://nextsp-server.id.vn/api/auth/register";
   const [fullName, setFullName] = useState("");
@@ -57,7 +58,8 @@ const RegisterForm = () => {
       confirmPassword,
     };
 
-    axios.post(apiUrl, postData)
+    axios
+      .post(apiUrl, postData)
       .then((response) => {
         console.log("Response data:", response.data);
         setFullName("");
@@ -66,7 +68,7 @@ const RegisterForm = () => {
         setPassword("");
         setConfirmPassword("");
         // Redirect to login page or do something else
-        loginPage('/loginUser');
+        loginPage("/loginUser");
       })
       .catch((error) => {
         console.error("Error:", error.response);
@@ -80,80 +82,115 @@ const RegisterForm = () => {
   };
 
   return (
-    <div className="w-full h-full flex justify-center items-center bg-gradient-to-tl from-login-pink via-login-blue to-login-green">
-      <div className="text-center w-[600px] max-h[900px] pt-[50px] px-10 border-2 border-white rounded-xl bg-[#ece9fe]/30 shadow-md">
-        <h1 className="text-4xl pb-5">Đăng ký</h1>
-        <div className="flex flex-col items-start">
-          <label className="ml-1 mb-2" htmlFor="firstName">Họ và tên: </label>
-          <input
-            id="firstName"
-            className="w-full py-2 px-3 mb-4 rounded-xl"
-            type="text"
-            placeholder="Tên Đầy Đủ"
-            value={fullName}
-            onChange={(e) => setFullName(e.target.value)}
-            onBlur={() => handleFieldBlur("fullName")}
-          />
-          {errors.fullName && <div className="text-red-500 float-right">{errors.fullName}</div>}
-
-          <label className="ml-1 mb-2" htmlFor="email">Email: </label>
-          <input
-            id="email"
-            className="w-full py-2 px-3 mb-4 rounded-xl"
-            type="email"
-            placeholder="Email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
-          {errors.email && <div className="text-red-500">{errors.email}</div>}
-
-          <label className="ml-1 mb-2" htmlFor="username">Tên tài khoản: </label>
-          <input
-            id="username"
-            className="w-full py-2 px-3 mb-4 rounded-xl"
-            type="text"
-            placeholder="Username"
-            value={accountName}
-            onChange={(e) => setAccountName(e.target.value)}
-          />
-          {errors.accountName && <div className="text-red-500 float-right">{errors.accountName}</div>}
-
-          <label className="ml-1 mb-2" htmlFor="password">Tạo mật khẩu: </label>
-          <input
-            id="password"
-            className="w-full py-2 px-3 mb-4 rounded-xl"
-            type="password"
-            placeholder="Mật khẩu"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
-          {errors.password && <div className="text-red-500 float-right">{errors.password}</div>}
-
-          <label className="ml-1 mb-2" htmlFor="confirmPassword">Xác nhận mật khẩu: </label>
-          <input
-            id="confirmPassword"
-            className="w-full py-2 px-3 mb-4 rounded-xl"
-            type="password"
-            placeholder="Nhập lại mật khẩu"
-            value={confirmPassword}
-            onChange={(e) => setConfirmPassword(e.target.value)}
-          />
-          {errors.confirmPassword && <div className="text-red-500 float-right">{errors.confirmPassword}</div>}
-
-          {errors.general && <div className="text-red-500">{errors.general}</div>}
-
-          <button
-            className="px-3 py-2 rounded w-full text-white my-3 mt-5 bg-blue-700"
-            onClick={handleRegister}
-          >
-            Tiếp tục
-          </button>
-
-          <p className="py-4">
-            Bạn đã có tài khoản? <Link to='/login' className="text-blue-700">Đăng nhập ngay</Link>
-          </p>
+    <div className=" w-full flex flex-col justify-center items-center bg-gradient-to-tl bg-color">
+      <div className="logoSection w-full bg-black">
+        <div className="logo h-24 w-24">
+          <img src={Logo} alt="" />
         </div>
       </div>
+      <div className="RegistSection relative ">
+        <div className="Form absolute right-32 top-10 text-center pt-[50px] px-10 border-2 border-white rounded-xl bg-white">
+          <h1 className="text-4xl pb-5">Đăng ký</h1>
+          <div className="flex flex-col items-start">
+            <label className="ml-1 mb-2" htmlFor="firstName">
+              Họ và tên:{" "}
+            </label>
+            <input
+              id="firstName"
+              className="w-full py-2 px-3 mb-4 rounder-sm"
+              type="text"
+              placeholder="Tên Đầy Đủ"
+              value={fullName}
+              onChange={(e) => setFullName(e.target.value)}
+              onBlur={() => handleFieldBlur("fullName")}
+            />
+            {errors.fullName && (
+              <div className="text-red-500 float-right">{errors.fullName}</div>
+            )}
+
+            <label className="ml-1 mb-2" htmlFor="email">
+              Email:{" "}
+            </label>
+            <input
+              id="email"
+              className="w-full py-2 px-3 mb-4 rounder-sm"
+              type="email"
+              placeholder="Email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+            />
+            {errors.email && <div className="text-red-500">{errors.email}</div>}
+
+            <label className="ml-1 mb-2" htmlFor="username">
+              Tên tài khoản:{" "}
+            </label>
+            <input
+              id="username"
+              className="w-full py-2 px-3 mb-4 rounder-sm"
+              type="text"
+              placeholder="Tải Khoản"
+              value={accountName}
+              onChange={(e) => setAccountName(e.target.value)}
+            />
+            {errors.accountName && (
+              <div className="text-red-500 float-right">
+                {errors.accountName}
+              </div>
+            )}
+
+            <label className="ml-1 mb-2" htmlFor="password">
+              Tạo mật khẩu:{" "}
+            </label>
+            <input
+              id="password"
+              className="w-full py-2 px-3 mb-4 rounder-sm"
+              type="password"
+              placeholder="Mật khẩu"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            {errors.password && (
+              <div className="text-red-500 float-right">{errors.password}</div>
+            )}
+
+            <label className="ml-1 mb-2" htmlFor="confirmPassword">
+              Xác nhận mật khẩu:{" "}
+            </label>
+            <input
+              id="confirmPassword"
+              className="w-full py-2 px-3 mb-4 rounder-sm"
+              type="password"
+              placeholder="Nhập lại mật khẩu"
+              value={confirmPassword}
+              onChange={(e) => setConfirmPassword(e.target.value)}
+            />
+            {errors.confirmPassword && (
+              <div className="text-red-500 float-right">
+                {errors.confirmPassword}
+              </div>
+            )}
+
+            {errors.general && (
+              <div className="text-red-500">{errors.general}</div>
+            )}
+
+            <button
+              className="px-3 py-2 rounded w-full text-white my-3 mt-5 btn"
+              onClick={handleRegister}
+            >
+              Tiếp tục
+            </button>
+
+            <p className="py-4">
+              Bạn đã có tài khoản?{" "}
+              <Link to="/login" className=" text-blue-400">
+                Đăng nhập ngay
+              </Link>
+            </p>
+          </div>
+        </div>
+      </div>
+      <Footer></Footer>
     </div>
   );
 };
