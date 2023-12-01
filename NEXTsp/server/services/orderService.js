9209369f7ad49f9ff97100832b137e61b2b0a8bd
xuandd <103272537+xuan3chan@users.noreply.github.com>
2023-12-01 17:16:52 +0700

    Add time to the createdAt date format

--- NEXTsp/server/services/orderService.js
+++ NEXTsp/server/services/orderService.js
@@ -82,8 +82,11 @@ class OrderService {
       const day = String(date.getDate()).padStart(2, '0');
       const month = String(date.getMonth() + 1).padStart(2, '0'); // Months are 0-based
       const year = date.getFullYear();
+      const hours = String(date.getHours()).padStart(2, '0');
+      const minutes = String(date.getMinutes()).padStart(2, '0');
+      const seconds = String(date.getSeconds()).padStart(2, '0');
 
-      return `${day}/${month}/${year}`;
+      return `${day}/${month}/${year} ${hours}:${minutes}:${seconds}`;
     };
 
     // Format the createdAt date for each order
